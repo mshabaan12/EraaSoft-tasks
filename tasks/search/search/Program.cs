2d7a473dc37e69752981588892d8bacc42384d64
mshabaan12 <mshabaan1231@outlook.com>
2025-07-27 02:33:53 +0300

    delete searchStardar

---
+++ tasks/search/search/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double discount = 0.25;
+            Console.WriteLine($"discount:{discount:p}");
+
+        }
+    }
+}
