e191c7cc02443f38629379a5ef611efc6f6c44e5
mshabaan12 <mshabaan1231@outlook.com>
2025-07-27 01:22:12 +0300

    newTask

---
+++ tasks/search 1/search 1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace search_1
+{
+    internal class Program
+    {
+        static void Main(string[] args, string value)
+        {
+            Console.WriteLine("Hello, World!");
+            int x = 10;
+            int y = 20;
+            Console.WriteLine($"equation: {x} + {y} = {x + y:c}");
+        }
+    }
+}
