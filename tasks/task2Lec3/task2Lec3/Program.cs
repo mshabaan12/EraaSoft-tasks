9d5015058ebe02dc1645b68c2aa52cfaaeb6bf86
mshabaan12 <mshabaan1231@outlook.com>
2025-08-03 00:58:19 +0300

    commit

---
+++ tasks/task2Lec3/task2Lec3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace task2Lec3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// multiplication table 
+            Console.WriteLine("Enter a number to print its multiplication table ");
+            int num =Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+            while (i <= 12)
+            {
+                Console.WriteLine(i + "*" + num + "=" + i * num);
+                i++;
+            }
+        }
+    }
+}
